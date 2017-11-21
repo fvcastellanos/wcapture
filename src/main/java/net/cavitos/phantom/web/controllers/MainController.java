fc10caa2288223c93125e1a0b611ab26a1a48254
fcastellanos <fcastellanos@paypal.com>
2017-11-20 19:16:11 -0600

    add basic project configuration

---
+++ src/main/java/net/cavitos/phantom/web/controllers/MainController.java
@@ -0,0 +1,18 @@
+package net.cavitos.phantom.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView main() {
+        return new ModelAndView("main");
+    }
+
+    
+
+}
---
+++ src/main/resources/templates/main.ftl
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>Welcome!</title>
+</head>
+<body>
+  <h1>Welcome!</h1>
+  <p>Our latest product:
+  <a href="#">Hola Mundo</a>!
+</body>
+</html>
