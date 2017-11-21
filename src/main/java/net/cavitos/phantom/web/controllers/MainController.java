f5f1770cd3af3b55d02e28d914d37b51945b0459
Francisco Castellanos <fvcastellanos@gmail.com>
2017-11-21 00:06:13 -0600

    add custom css, prepare view template

--- src/main/java/net/cavitos/phantom/web/controllers/MainController.java
+++ src/main/java/net/cavitos/phantom/web/controllers/MainController.java
@@ -1,9 +1,14 @@
 package net.cavitos.phantom.web.controllers;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.common.collect.Maps;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -13,6 +18,17 @@ public class MainController {
         return new ModelAndView("main");
     }
 
-    
+    @RequestMapping(value = "/capture", method = RequestMethod.POST)
+    public ModelAndView capture(String url) {
+
+        if (StringUtils.isEmpty(url)) {
+            Map<String, String> map = Maps.newHashMap();
+            map.put("error", "Please provide a valid URL");
+
+            return new ModelAndView("main", map);
+        }
+
+        return new ModelAndView("main");
+    }
 
 }
--- src/main/resources/templates/main.ftl
+++ src/main/resources/templates/main.ftl
@@ -1,10 +1,23 @@
-<html>
-<head>
-  <title>Welcome!</title>
-</head>
-<body>
-  <h1>Welcome!</h1>
-  <p>Our latest product:
-  <a href="#">Hola Mundo</a>!
-</body>
-</html>
+<#assign content>
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Welcome!</h3>
+            <p>Please enter the URL of the site you want to capture</p>
+            <form action="/capture" method="post">
+                <div class="form-group">
+                    <label for="url">URL</label>
+                    <input type="text" name="url" placeholder="URL" class="form-control" value="" />
+                </div>
+
+                <input type="submit" class="btn btn-primary" value="Capture URL" />
+            </form>
+
+            <#if error??>
+                <p>${error}</p>
+            </#if>
+
+        </div>
+    </div>
+</#assign>
+
+<#include "template.ftl"/>
