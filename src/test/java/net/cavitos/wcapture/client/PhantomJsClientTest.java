a85c10f805551085e4268aa0ac46a8f3fe2c46a5
Jorge Alvarez <alvarez.jeap@gmail.com>
2019-10-26 13:25:46 -0600

    Update services

--- src/test/java/net/cavitos/wcapture/client/PhantomJsClientTest.java
+++ src/test/java/net/cavitos/wcapture/client/PhantomJsClientTest.java
@@ -11,30 +11,30 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PhantomJsClientTest {
 
-//    private PhantomJsClient phantomJsClient;
-//
-//    @BeforeEach
-//    void setUp() {
-//        phantomJsClient = new PhantomJsClient("https://phantom:8910",
-//                                              System.getProperty("java.io.tmpdir"));
-//    }
-//
-//    @Test
-//    void testCreateWebDriver() throws MalformedURLException {
-//        final var webDriver = phantomJsClient.createWebDriver();
-//
-//        assertThat(webDriver).isNotNull();
-//    }
-//
-//    @Test
-//    void testTakeScreenshot() throws IOException {
-//        final var webDriver = phantomJsClient.createWebDriver();
-//        phantomJsClient.takeScreenshot(webDriver, "");
-//    }
-//
-//    @Test
-//    void testGetScreenshot() throws FileNotFoundException {
-//        phantomJsClient.getScreenshot("");
-//    }
+    private PhantomJsClient phantomJsClient;
+
+    @BeforeEach
+    void setUp() {
+        phantomJsClient = new PhantomJsClient("https://phantom:8910",
+                                              System.getProperty("java.io.tmpdir"));
+    }
+
+    @Test
+    void testCreateWebDriver() throws MalformedURLException {
+        final var webDriver = phantomJsClient.createWebDriver();
+
+        assertThat(webDriver).isNotNull();
+    }
+
+    @Test
+    void testTakeScreenshot() throws IOException {
+        final var webDriver = phantomJsClient.createWebDriver();
+        phantomJsClient.takeScreenshot(webDriver, "");
+    }
+
+    @Test
+    void testGetScreenshot() throws FileNotFoundException {
+        phantomJsClient.getScreenshot("");
+    }
 
 }
